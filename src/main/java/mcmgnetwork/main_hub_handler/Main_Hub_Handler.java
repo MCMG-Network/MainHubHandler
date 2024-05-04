5022f4e6fb5eeea298ae007950df5b86f9e88bb6
mbovero <milesbovero@icloud.com>
2024-05-03 21:30:45 -0600

    Initial commit.

---
+++ src/main/java/mcmgnetwork/main_hub_handler/Main_Hub_Handler.java
@@ -0,0 +1,17 @@
+package mcmgnetwork.main_hub_handler;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main_Hub_Handler extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
