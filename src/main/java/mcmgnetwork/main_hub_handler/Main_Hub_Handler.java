a4ca2aceee6031bdfcfb5bbd6ba272d7eac01f63
mbovero <milesbovero@icloud.com>
2024-05-04 12:24:54 -0600

    Began development on grief prevention. Players can no longer interact with armor stands.

--- src/main/java/mcmgnetwork/main_hub_handler/Main_Hub_Handler.java
+++ src/main/java/mcmgnetwork/main_hub_handler/Main_Hub_Handler.java
@@ -1,13 +1,15 @@
 package mcmgnetwork.main_hub_handler;
 
+import mcmgnetwork.main_hub_handler.listeners.PlayerGriefListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main_Hub_Handler extends JavaPlugin {
 
     @Override
-    public void onEnable() {
-        // Plugin startup logic
-
+    public void onEnable()
+    {
+        // Register event listeners/handlers
+        getServer().getPluginManager().registerEvents(new PlayerGriefListener(), this);
     }
 
     @Override
---
+++ src/main/java/mcmgnetwork/main_hub_handler/listeners/PlayerGriefListener.java
@@ -0,0 +1,16 @@
+package mcmgnetwork.main_hub_handler.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
+
+public class PlayerGriefListener implements Listener {
+    @EventHandler
+    public void onArmorStandManipulate(PlayerArmorStandManipulateEvent e)
+    {
+        Player player = e.getPlayer();
+        player.sendMessage("Hey! Don't touch that!");
+        e.setCancelled(true);
+    }
+}
