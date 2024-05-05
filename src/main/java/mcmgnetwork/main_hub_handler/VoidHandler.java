fa743104d13afe0c3ef060bd3f98f983563d7ae4
mbovero <milesbovero@icloud.com>
2024-05-05 13:31:22 -0600

    Renamed and began implementing hub boundaries

--- src/main/java/mcmgnetwork/main_hub_handler/VoidHandler.java
+++ src/main/java/mcmgnetwork/main_hub_handler/VoidHandler.java
@@ -1,5 +1,7 @@
 package mcmgnetwork.main_hub_handler;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -7,7 +9,7 @@ import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
-public class VoidLevitationHandler implements Listener
+public class VoidHandler implements Listener
 {
 
     public static final int voidHeight = 0;
@@ -22,7 +24,22 @@ public class VoidLevitationHandler implements Listener
     public void onEnterVoid(PlayerMoveEvent e)
     {
         Player player = e.getPlayer();
+
+        // Give the player levitation if they fall into the void
         if (e.getTo().getY() < voidHeight)
             player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, levitationDuration, levitationAmplifier));
+
+        try
+        {
+            // Initialize world spawn location
+            Location spawn = new Location(Bukkit.getWorld("world"), HubProperties.worldSpawnX, HubProperties.worldSpawnY, HubProperties.worldSpawnZ);
+
+            // If players fall too far into the void, tp them back to the world spawn
+
+            // If players venture too far in the X direction, tp them back to the world spawn
+
+            // If players venture too far in the Z direction, tp them back to the world spawn
+        }catch (NullPointerException ex)
+        { Bukkit.getLogger().severe("ERROR: Could not update main hub game rules because no world file was found!"); }
     }
 }
