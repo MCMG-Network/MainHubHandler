5677560c0ba9bf4fa5ac62c62653b422c9557b06
mbovero <milesbovero@icloud.com>
2024-05-06 21:01:47 -0600

    Created class to store all server names recognized by proxy server

---
+++ src/main/java/mcmgnetwork/main_hub_handler/ServerNames.java
@@ -0,0 +1,15 @@
+package mcmgnetwork.main_hub_handler;
+
+/**
+ * Description: <p>
+ *  Stores all official server name strings recognized by the Velocity proxy server and used for player transferring.
+ *
+ *  <p>Author(s): Miles Bovero
+ *  <p>Date Created: 5/6/24
+ */
+public class ServerNames
+{
+    public static final String mainHub = "main_hub";
+    public static final String KOTH_Lobby = "KOTH_lobby";
+    public static final String MM_Lobby = "MM_lobby";
+}
