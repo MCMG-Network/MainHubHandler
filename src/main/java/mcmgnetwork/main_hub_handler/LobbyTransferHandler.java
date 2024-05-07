a7d140fad0314edbd1627aae7868d40f153d5df3
mbovero <milesbovero@icloud.com>
2024-05-06 20:59:22 -0600

    Renamed file, refactored messaging strings, and improved commenting

--- src/main/java/mcmgnetwork/main_hub_handler/LobbyTransferHandler.java
+++ src/main/java/mcmgnetwork/main_hub_handler/LobbyTransferHandler.java
@@ -9,35 +9,38 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 /**
  * Description: <p>
+ *  Utilizes plugin messaging to send requests to the proxy server to transfer players to other servers.
  *
  *  <p>Author(s): Miles Bovero, Vee Sutton, Lawrence Ponce
  *  <p>Date Created: 5/5/24
  */
-public class LobbySwitcher implements Listener
+public class LobbyTransferHandler implements Listener
 {
 
+    private static final String transferMessageType = "ConnectOther";
+    private static final String transferChannelName = "BungeeCord";
     private static JavaPlugin plugin;
 
     /**
-     * Setter method for the JavaPlugin (MainHubHandler) reference used to transfer players to other lobbies.
+     * Setter method for the JavaPlugin (MainHubHandler) reference necessary for transferring players to other lobbies.
      * @param plugin The MainHubHandler JavaPlugin reference
      */
-    public static void SetJavaPlugin (JavaPlugin plugin) { LobbySwitcher.plugin = plugin; }
+    public static void SetJavaPlugin (JavaPlugin plugin) { LobbyTransferHandler.plugin = plugin; }
 
     /**
-     * Sends the *Player* to the specified server.
+     * Sends the specified Player to the specified server.
      * @param serverName The name of the server to transfer the player to
-     * @param player The *Player* object to be transferred
+     * @param player The Player object to be transferred
      */
     public static void Transfer (String serverName, Player player)
     {
         // Prepare plugin message: Message Type = "ConnectOther"; Arguments = "playerName", "serverName"
         ByteArrayDataOutput out = ByteStreams.newDataOutput();
-        out.writeUTF("ConnectOther");
+        out.writeUTF(transferMessageType);
         out.writeUTF(player.getName());
         out.writeUTF(serverName);
 
-        // Send the *Player* to the respective lobby
-        plugin.getServer().sendPluginMessage(plugin, "BungeeCord", out.toByteArray());
+        // Send the Player to the respective lobby
+        plugin.getServer().sendPluginMessage(plugin, transferChannelName, out.toByteArray());
     }
 }
