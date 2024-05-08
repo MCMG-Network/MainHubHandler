da1739478bf4daadd2d5336536ff1148eb523745
mbovero <milesbovero@icloud.com>
2024-05-07 21:50:32 -0600

    Created file containing commonly used plugin messaging channel names

---
+++ src/main/java/mcmgnetwork/main_hub_handler/protocols/ChannelNames.java
@@ -0,0 +1,22 @@
+package mcmgnetwork.main_hub_handler.protocols;
+
+/**
+ * Description: <p>
+ *  Stores plugin messaging channel names for easy reference across the plugin.
+ *
+ *  <p>Author(s): Miles Bovero
+ *  <p>Date Created: 5/7/24
+ */
+public class ChannelNames
+{
+    /**
+     * The BungeeCord-supported plugin messaging channel used for proxy server interactions.
+     */
+    public static final String PROXY = "BungeeCord";
+
+    /**
+     * The custom MCMG plugin messaging channel used for network server handling and interaction.
+     */
+    public static final String MCMG = "mcmg:main";
+
+}
