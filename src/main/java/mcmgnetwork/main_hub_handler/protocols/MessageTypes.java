bdb8f9306fec50655f4149b540dff05dec81a1bd
mbovero <milesbovero@icloud.com>
2024-05-07 23:20:38 -0600

    Updated documentation/implementation

--- src/main/java/mcmgnetwork/main_hub_handler/protocols/MessageTypes.java
+++ src/main/java/mcmgnetwork/main_hub_handler/protocols/MessageTypes.java
@@ -15,8 +15,19 @@ public class MessageTypes
     public static final String TRANSFER = "ConnectOther";
 
     /**
-     * Causes the proxy server to return a "ServerStatus" plugin message containing the status of the specified server
-     * type and an online server's name if any server of that type is online.
+     * DataOutput should contain SERVER_TRANSFER_REQUEST, [player to be transferred], [server type to transfer to]
+     * <p>
+     * Causes the proxy server to return a SERVER_TRANSFER_RESPONSE plugin message type.
      */
-    public static final String SERVER_STATUS_REQUEST = "ServerStatusRequest";
+    public static final String SERVER_TRANSFER_REQUEST = "ServerTransferRequest";
+
+    /**
+     * The message sent by the proxy server in response to a SERVER_TRANSFER_REQUEST, confirming whether or not to
+     * transfer the requesting player to another server.
+     * <p>
+     * Contains a boolean indicating whether or not an instance of the SERVER_TRANSFER_REQUEST's specified server type
+     * is online, the player to be transferred, and - if any server of that type is online - the name of an online
+     * server to transfer the player to.
+     */
+    public static final String SERVER_TRANSFER_RESPONSE = "ServerTransferResponse";
 }
