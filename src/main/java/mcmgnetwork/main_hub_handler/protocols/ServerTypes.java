4dca4a05fff9c6ed2b92bdb48af6571347a3c49d
mbovero <milesbovero@icloud.com>
2024-05-07 21:41:16 -0600

    Moved plugin messaging-related files to a protocols folder

---
+++ src/main/java/mcmgnetwork/main_hub_handler/protocols/ServerTypes.java
@@ -0,0 +1,15 @@
+package mcmgnetwork.main_hub_handler.protocols;
+
+/**
+ * Description: <p>
+ *  Stores MCMG network server type/category strings recognized by the Velocity proxy server's MCMG_NetworkHandler
+ *  plugin and used for plugin messaging.
+ *
+ *  <p>Author(s): Miles Bovero
+ *  <p>Date Created: 5/6/24
+ */
+public class ServerTypes
+{
+    public static final String KOTH = "KOTH";
+    public static final String MM = "MM";
+}
